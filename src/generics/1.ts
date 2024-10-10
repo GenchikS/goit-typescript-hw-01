418e381c31cf652dff46c98cd5564356e595469d
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-10-10 09:57:05 +0300

    v.1.1.3

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 
-async function fetchData(url:any) {
+async function fetchData(url:string) {
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const response:unknown = await axios.get(url);
+    if (typeof response === `object` && response !== null )
+    return response;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
